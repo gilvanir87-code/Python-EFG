0392da1a386b1ef4d88228ac777f2577991954df
Gilvanir-sousa <gilvanir87@gmail.com>
2026-06-24 19:06:33 -0300

    Refactor dice rolling function and update messages

--- Jogando o dado.py
+++ Jogando o dado.py
@@ -1,16 +1,16 @@
 import random
 
-def rolar_dado():
+def jogar_dado():
     return random.randint(1, 6)
 print("\n===================== Lançando o Dado =====================")
 while True:
     opcao = input("\nDigite 'jogar' para lançar o dado ou 'sair' para encerrar: ").lower()
 
     if opcao == "sair":
-        print("\nVocê saiu.")
+        print("\nVocê saiu! Até breve!")
         break
     elif opcao == "jogar":
-        resultado = rolar_dado()
+        resultado = jogar_dado()
         print(f"\nSaiu o número: {resultado}")
     else:
         print("\nOpção inválida. Jogue novamente.")
